2e54a13703e5471607c7649d4f6151ad2797a751
Khalil L <mortyl0834@gmail.com>
2023-05-27 23:16:14 +0800

    [Fix][script]:fix docker_swarm_test.py and docker_entrypoint.sh path error

--- scripts/docker_entrypoint.sh
+++ scripts/docker_entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 source /opt/ros/noetic/setup.bash
-source /home/xuhao/d2slam_ws/devel/setup.bash
+source /root/swarm_ws/devel/setup.bash
 echo "Run bag replay for drone",$DRONE_ID, "with bag", $1, "token", $2
 route add -net 224.0.0.0 netmask 240.0.0.0 dev eth0
 chmod a+x /root/entry_point.sh
